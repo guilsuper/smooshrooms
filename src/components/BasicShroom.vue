b28526d03a696bdb8aded42fe4fdc6a5775ddfa2
“guilsuper” <gm.may.power@gmail.com>
2022-11-02 23:13:49 -0600

    notes and static despawn time

--- src/components/BasicShroom.vue
+++ src/components/BasicShroom.vue
@@ -1,5 +1,7 @@
 <template>
+  <!-- image for the shroom, and the gif for smooshing -->
   <div draggable="false" class="small-box shroom" :class="'position' + bShroom.location" @click="hitShroom(bShroom.id)" >
+    <!-- location is a random number between 1-10 tied to a set of css props for location on the dom -->
     <img :id="'shroom'+bShroom.id" class="small-box btn m-0" draggable="false" title="basic shroom" src="../assets/brown-shroom.png" alt="Basic-Shroom">
     <img :id="'poof'+bShroom.id" class="small-box m-0 d-none" draggable="false" src="https://animated-gif-creator.com/images/01/the-one-who-got-away_86.gif" alt="poof">
   </div>
@@ -11,13 +13,18 @@ import { useToast } from "vue-toastification"
 import {mushroomsService} from "../service/mushroomsService.js"
 
 export default {
+  // the props contain the information given to each instance of this component
   props: {bShroom: { type: Object, required: true }},
+  // props are passed down to the setup for use inside my functions
 setup(props){
   const toast = useToast();
+  // each instance of the component loads with a despawn timer
   onMounted(()=> {
     setInterval(()=> {
+      // we pass the id from the props to the service in order to filter (despawn) the correct shroom
           mushroomsService.despawn(props.bShroom.id)
-        }, Math.random()*3000)
+        }, 1500)
+        // this used to be a random value but a set time is probably best for lvl 1
     })
 return{
   toast,
