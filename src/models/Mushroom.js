78c9a12d80f007103e740c490a0d5b460cf9735f
“guilsuper” <gm.may.power@gmail.com>
2022-11-06 22:33:44 -0700

    created mushroom model

---
+++ src/models/Mushroom.js
@@ -0,0 +1,12 @@
+export class Mushrooms{
+  constructor(data){
+    this.id = data.id
+    this.name = data.name
+    this.hitPoints = data.hitPoints
+    this.location = data.location
+    this.img = data.img
+    this.poofImg = data.poofImg
+    this.disabled = data.disabled
+    this.despawnDelay = data.despawnDelay
+    }
+  }
