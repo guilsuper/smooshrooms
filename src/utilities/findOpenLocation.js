364739060c028960a0fcf8f13f7e0238d854ff83
“guilsuper” <gm.may.power@gmail.com>
2022-11-07 16:26:45 -0700

    fixed again

--- src/utilities/findOpenLocation.js
+++ src/utilities/findOpenLocation.js
@@ -2,7 +2,7 @@ import $Store from '../Store.js'
 export function findOpenLocation() {
   let openLocations = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   for(let i = 0; i <= $Store.state.basicShrooms.length; i++){
-    openLocations = openLocations.filter(l => l != $Store.state.basicShrooms[i])
+    openLocations = openLocations.filter(l => l != $Store.state.basicShrooms.location[i])
   }
   const index = Math.floor(Math.random()*openLocations.length)
   const newLocation = openLocations[index]
