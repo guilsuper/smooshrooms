48609beaafc6d6981d6c2d9d1895c81fa430f548
“guilsuper” <gm.may.power@gmail.com>
2022-11-09 14:55:24 -0700

    gzfd

--- src/components/BasicShroom.vue
+++ src/components/BasicShroom.vue
@@ -30,7 +30,7 @@ setup(props){
     setTimeout(()=> {
       // we pass the id from the props to the service in order to filter (despawn) the correct shroom
           mushroomsService.determineDespawnScenario(props.bShroom.id)
-        }, 15000)
+        }, 1500)
         // this used to be a random value but a set time is probably best for lvl 1
     })
 return{
